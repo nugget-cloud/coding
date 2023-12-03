834b1f860789d99bf650f9f16e46416877d55bdb
Kunal <131735057+nugget-cloud@users.noreply.github.com>
2023-12-03 13:32:23 +0530

    Add files via upload

---
+++ k.py
@@ -0,0 +1,17 @@
+MOD = 10**9 + 7
+def ways(nvalues):
+    results = []
+    max_n = max(nvalues)
+    dp = [0] * (max_n+1)
+    dp[0] = 1  
+    dp[1] = 0  
+    if max_n >= 2:
+        dp[2] = 2  
+    if max_n >= 3:
+        dp[3] = 3 
+    for i in range(4, max_n+1):
+        dp[i] = (dp[i-1] + 2*dp[i-2] + 4*dp[i-3]) % MOD
+    for n in nvalues:
+        results.append(dp[n])
+    
+    return results
